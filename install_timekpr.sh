42a15bc94f7c5af9e3bccd4553e3a0a8ab884029
Savvas Radevic <vicedar@gmail.com>
2008-09-19 12:01:44 +0200

    Got install_timekpr.sh from http://www.nedberg.net/install_timekpr.sh

--- install_timekpr.sh
+++ install_timekpr.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-if [ ! -d /usr local/bin ]; then
+if [ ! -d /usr/local/bin ]; then
 	mkdir /usr/local/bin
 fi
-apt-get install libnotify-bin
-wget nedberg.net/timekpr/addtime.sh -O /usr/local/bin/addtime.sh
-wget nedberg.net/timekpr/extendlimits.sh -O /usr/local/bin/extendlimits.sh
-wget nedberg.net/timekpr/timekpr.sh -O /usr/local/bin/timekpr.sh
-wget nedberg.net/timekpr/timekprGUI.py -O /usr/local/bin/timekprGUI.py
-wget nedberg.net/timekpr/Timekpr.glade -O /usr/local/bin/Timekpr.glade
-chmod 744 /usr/local/bin/addtime.sh /usr/local/bin/extendlimits.sh /usr/local/bin/timekpr.sh /usr/local/bin/timekprGUI.py /usr/local/bin/Timekpr.glade
+if [ ! -d /usr/share/timekpr ]; then
+	mkdir /usr/share/timekpr
+fi
+wget nedberg.net/timekpr/timekpr-addtime -O /usr/local/bin/timekpr-addtime
+wget nedberg.net/timekpr/timekpr-extend -O /usr/local/bin/timekpr-extend
+wget nedberg.net/timekpr/timekpr -O /usr/local/bin/timekpr
+wget nedberg.net/timekpr/timekpr-gui -O /usr/local/bin/timekpr-gui
+wget nedberg.net/timekpr/timekpr.glade -O /usr/share/timekpr/timekpr.glade
+chmod 744 /usr/local/bin/timekpr-addtime /usr/local/bin/timekpr-extend /usr/local/bin/timekpr /usr/local/bin/timekpr-gui
 if [ ! -d /var/lib/timekpr ]; then
 	mkdir /var/lib/timekpr
 fi
