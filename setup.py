efcad0b81f8b733547f0e9cbf773b82d3f67d78c
Savvas Radevic <vicedar@gmail.com>
2008-10-19 16:43:20 +0200

    * Pushed version 0.2.2 (so I could set it as beta 1 release) * debian/control: Minor clean-up

--- setup.py
+++ setup.py
@@ -3,3 +3,19 @@ import py_compile
 py_compile.compile("timekprpam.py")
 py_compile.compile("timekprcommon.py")
 
+#from distutils.core import setup
+
+#setup(
+#	name='timekpr',
+#	version='0.2.1',
+#	description='Keep control of computer usage',
+#	long_description='This program will track and control the computer usage of \
+#your user accounts. You can limit their daily usage based \
+#on a timed access duration and configure periods of day \
+#when they can or cannot log in.',
+#	maintainer='Savvas Radevic',
+#	maintainer_email='vicedar@gmail.com',
+#	url='http://launchpad.net/timekpr',
+#	packages=['timekpr']
+#)
+
