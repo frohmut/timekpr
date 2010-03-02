ee3c73882035bd35f3de4420be41cb0829795085
Savvas Radevic <vicedar@gmail.com>
2010-03-02 20:02:28 +0100

    locked setup.py again

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#exit("trunk is unstable -- you should not use it to install/test timekpr!")
+exit("trunk is unstable -- you should not use it to install/test timekpr!")
 
 import sys
 import os
